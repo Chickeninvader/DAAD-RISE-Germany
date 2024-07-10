8f1c1aec0a404a7b99355ae93a5bd158502fac7a
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-10 12:59:15 +0200

    fix bug

---
+++ train.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#SBATCH --partition=compute # partition
+#SBATCH --nodes=1  # number of nodes
+#SBATCH --tasks-per-node=1  # number of tasks per node
+#SBATCH --cpus-per-task 1  # number of cpus per task
+#SBATCH --gres=gpu:1 # number of gpus (4 out of 8)
+#SBATCH --mem=10000  # memory pool for all cores (in megabytes, if w/o suffix)
+#SBATCH -t 0-10:00  # time (D-HH:MM)
+#SBATCH -o /home/nvo/slurm_logs/slurm.%N.%j.out  # STDOUT
+#SBATCH -e /home/nvo/slurm_logs/slurm.%N.%j.err  # STDERR
+# show visible gpus
+echo $CUDA_VISIBLE_DEVICES
+# add path to micromamba
+PATH=/usr/local/bin:$PATH
+source ~/.bashrc
+eval "$(micromamba shell hook --shell bash)"
+micromamba activate daad
+echo Starting Train
+date
+## < Here comes the command to be executed >
+python critical_classification/training_tensorflow.py
+
+echo Training complete
+date
