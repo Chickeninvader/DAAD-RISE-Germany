90c66912b4e7cd0a3544818e070385738b22052f
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-11 14:21:49 +0200

    fix bugs

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1  # number of nodes
 #SBATCH --tasks-per-node=1  # number of tasks per node
 #SBATCH --cpus-per-task 1  # number of cpus per task
-#SBATCH --gres=gpu:1 # number of gpus (4 out of 8)
+#SBATCH --gres=gpu:0 # number of gpus (4 out of 8)
 #SBATCH --mem=10000  # memory pool for all cores (in megabytes, if w/o suffix)
 #SBATCH -t 0-03:00  # time (D-HH:MM)
 #SBATCH -o /home/nvo/slurm_logs/slurm.%N.%j.out  # STDOUT
