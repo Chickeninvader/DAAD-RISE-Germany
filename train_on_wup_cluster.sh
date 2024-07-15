01c81ef8c565420a79d99c209d7f07d0709a37f9
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-15 17:00:00 +0200

    fix bugs

--- train_on_wup_cluster.sh
+++ train_on_wup_cluster.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task 1  # number of cpus per task
 #SBATCH --gres=gpu:1 # number of gpus (4 out of 8)
 #SBATCH --mem=10000  # memory pool for all cores (in megabytes, if w/o suffix)
-#SBATCH -t 0-05:00  # time (D-HH:MM)
+#SBATCH -t 0-02:00  # time (D-HH:MM)
 #SBATCH -o /home/nvo/slurm_logs/slurm.%N.%j.out  # STDOUT
 #SBATCH -e /home/nvo/slurm_logs/slurm.%N.%j.err  # STDERR
 # show visible gpus
