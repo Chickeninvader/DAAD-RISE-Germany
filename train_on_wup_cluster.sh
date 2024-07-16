05f09676655002497bcdff5a33bd0bad0bd7c2d8
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-16 11:22:04 +0200

    fix bugs

--- train_on_wup_cluster.sh
+++ train_on_wup_cluster.sh
@@ -20,7 +20,7 @@ date
 ## < Here comes the command to be executed >
 
 # For training the model
-python critical_classification/training_torch.py --data_location /data/nvo/original_video/
+#python critical_classification/training_torch.py --data_location /data/nvo/original_video/
 
 # For visualization
 python critical_classification/inference_with_model.py --data_location /data/nvo/original_video/
