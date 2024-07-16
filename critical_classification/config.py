63687f4ab27e18196212917cfa6ef4c592984c79
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-16 16:44:00 +0200

    fix bugs

--- critical_classification/config.py
+++ critical_classification/config.py
@@ -45,7 +45,7 @@ class Config:
         # self.pretrained_path = 'critical_classification/save_models/file_name'
 
         # Scheduler: step, exponential or cosine
-        self.scheduler = 'exponential'
+        self.scheduler = 'cosine'
 
         # Flag to indicate whether to save the trained model
         self.save_files = True
