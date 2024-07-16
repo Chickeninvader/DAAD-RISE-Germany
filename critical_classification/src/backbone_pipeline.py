efe34e7b72515342264833e41bc351b9057a2c1f
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-16 16:43:10 +0200

    fix bugs

--- critical_classification/src/backbone_pipeline.py
+++ critical_classification/src/backbone_pipeline.py
@@ -27,7 +27,10 @@ def initiate(config: Config):
     model_name = config.model_name
     pretrained_path = config.pretrained_path
     sample_duration = config.sample_duration
-    device_str = config.device_str
+    if config.device_str == 'cuda:0' and torch.cuda.is_available():
+        device_str = config.device_str
+    else:
+        device_str = 'cpu'
     # device_str = 'mps' if utils.is_local() and torch.backends.mps.is_available() \
     #     else ("cuda:0" if torch.cuda.is_available() else 'cpu')
 
