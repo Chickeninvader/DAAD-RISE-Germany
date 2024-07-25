bd4107d3bdbf118f1d2e49d3650e09d176eecf2c
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-25 16:42:15 +0200

    fix bugs

--- train_on_wup_cluster.sh
+++ train_on_wup_cluster.sh
@@ -21,9 +21,10 @@ date
 
 # For training the model
 python critical_classification/training_torch.py \
-  --model_name Swin3D \
+  --model_name YOLOv1_video \
   --data_location /data/nvo/ \
-  --image_batch_size 10
+  --image_batch_size 10 \
+  --additional_config no_fc
 
 # For visualization
 
