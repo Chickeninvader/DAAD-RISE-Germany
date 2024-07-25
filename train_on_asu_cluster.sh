eeeea85bd5a17b15978a75e2265ffa5c85502534
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-07-25 13:47:33 +0200

    fix bugs

--- train_on_asu_cluster.sh
+++ train_on_asu_cluster.sh
@@ -3,12 +3,12 @@
 # Reference: https://asurc.atlassian.net/wiki/spaces/RC/pages/1908998178/Sol+Hardware+-+How+to+Request
 #SBATCH -N 1            # number of nodes
 #SBATCH -c 2            # number of cores
-#SBATCH -G 1            # Request 1 gpu
+#SBATCH -G a100:1            # Request 1 gpu
 #SBATCH -t 0-0:30:00   # time in d-hh:mm:ss
 #SBATCH -p general      # partition
 #SBATCH -q public       # QOS
-#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
-#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH -o slurm_logs/slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm_logs/slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
 #SBATCH --mail-user="%u@asu.edu"
 #SBATCH --export=NONE   # Purge the job-submitting shell environment
@@ -27,18 +27,18 @@ date
 ## < Here comes the command to be executed >
 
 # For training the model
-#python critical_classification/training_torch.py \
-#  --model_name Swin3D \
-#  --data_location /data/nvo/ \
-#  --image_batch_size 10
+python critical_classification/training_torch.py \
+  --model_name Swin3D \
+  --data_location /data/nvo/ \
+  --image_batch_size 10
 
 # For visualization
 
-python critical_classification/inference.py \
-  --model_name YOLOv1_video \
-  --data_location /data/nvo/ \
-  --pretrained_path critical_classification/save_models/Dall_MSwin3D_lr1e-05_lossBCE_e10_scosine_Aexperiment_20240724_010605.pth \
-  --infer_all_video
+#python critical_classification/inference.py \
+#  --model_name YOLOv1_video \
+#  --data_location /data/nvo/ \
+#  --pretrained_path critical_classification/save_models/Dall_MSwin3D_lr1e-05_lossBCE_e10_scosine_Aexperiment_20240724_010605.pth \
+#  --infer_all_video
 
 
 # For other purpose
