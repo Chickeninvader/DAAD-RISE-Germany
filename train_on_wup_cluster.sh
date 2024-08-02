34acd329fe0cb313596f6a5d63b09c543d67f5af
Chickeninvader <96258758+Chickeninvader@users.noreply.github.com>
2024-08-02 13:45:28 +0200

    fix bugs

--- train_on_wup_cluster.sh
+++ train_on_wup_cluster.sh
@@ -33,19 +33,19 @@ date
 
 # For visualization
 
-python critical_classification/inference.py \
-  --model_name YOLOv1_video \
-  --data_location /data/nvo/ \
-  --image_batch_size 10 \
-  --pretrained_path critical_classification/save_models/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143640_/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143640__e19_best.pth \
-  --infer_all_video
-
 #python critical_classification/inference.py \
 #  --model_name YOLOv1_video \
 #  --data_location /data/nvo/ \
 #  --image_batch_size 10 \
-#  --pretrained_path critical_classification/save_models/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143712_no_fc/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143712_no_fc_e10_best.pth \
+#  --pretrained_path critical_classification/save_models/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143640_/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143640__e19_best.pth \
 #  --infer_all_video
+
+python critical_classification/inference.py \
+  --model_name YOLOv1_video \
+  --data_location /data/nvo/ \
+  --image_batch_size 10 \
+  --pretrained_path critical_classification/save_models/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143712_no_fc/Dall_MYOLOv1_video_lr0.0001_lossBCE_e20_scosine_Aexperiment_20240729_143712_no_fc_e10_best.pth \
+  --infer_all_video
 
 # For other purpose
 # download dataset
